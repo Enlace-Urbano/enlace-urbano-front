0e9c2da6146205f8440f3218ea89ca8d82574478
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-03-07 15:18:13 +0100

    created footer component

---
+++ src/components/Footer/FooterStyle.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const FooterStyle=styled.footer`
+display: flex;
+background-color: #000000;
+align-items:center;
+justify-content: space-around;
+
+p{
+
+}
