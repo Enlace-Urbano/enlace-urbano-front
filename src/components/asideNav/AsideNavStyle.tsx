0ce6ea3b9e511ae56ba448d3604153bb0b6503d5
gbixio <galbixio93@gmail.com>
2023-03-27 11:50:19 +0200

    aside nav

---
+++ src/components/asideNav/AsideNavStyle.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const AsideNavStyle = styled.nav`
+position: fixed;
+display: flex;
+flex-direction: column;
+gap: 0.5em;
+right: 0;
+bottom: 40%;
+background-color: var(--color-blue);
+border-radius: 5px 0 0 5px  ;
+padding: 0.5em;
+z-index: 1;
+
+svg {
+padding: 0.2em;
+ width: 30px;
+ height: 30px;
+ color: var(--color-white);
+}
+svg:hover {
+    transition: 0.2s;
+    color: #00B899;
+}
+`  
--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -4,6 +4,5 @@ const Contact = () => {
     return (
         <h1>contact</h1>
     )
-
 }
 export default Contact
