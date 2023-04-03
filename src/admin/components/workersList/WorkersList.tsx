45a226be1b9417b40fcfb5e05f4440593e61c6dc
Emmanuel <116069729+Rguzman77@users.noreply.github.com>
2023-04-03 20:19:36 +0200

    removed console logs

--- src/admin/components/workersList/WorkersList.tsx
+++ src/admin/components/workersList/WorkersList.tsx
@@ -16,7 +16,6 @@ const WorkersList = () => {
     getWorkersRequest()
       .then(response => {
         setWorkers(response.data);
-        console.log(response.data);
       })
       .catch(error => {
         console.error(error);
